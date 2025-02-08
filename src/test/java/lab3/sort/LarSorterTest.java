54e4b09d49a7c1ac765d5d047be9e3cd5ab91870
Ikram0990 <ikramv27@gmail.com>
2025-02-08 04:21:09 +0300

    Update LarSorterTest.java

--- src/test/java/lab3/sort/LarSorterTest.java
+++ src/test/java/lab3/sort/LarSorterTest.java
@@ -1,5 +1,21 @@
 package lab3.sort;
 
+public class LabSorter {
+    
+    public int[] sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
